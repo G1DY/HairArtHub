801b500273af91e8de7236e4cb292c105d750cb9
Gideon <kipkerring.gedion@gmail.com>
2023-12-07 19:27:20 +0300

    updated auth.py, models.py and views.py

--- hairArtProject/models.py
+++ hairArtProject/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from hairArtProject import db 
+from hairArtProject import db
 
 
 class Customer(db.Model):
-    '''the customer table'''
+    """the customer table"""
+
     customer_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
@@ -15,7 +16,8 @@ class Customer(db.Model):
 
 
 class Sevices(db.Model):
-    '''the services available'''
+    """the services available"""
+
     service_id = db.Column(db.Integer, primary_key=True)
     service_name = db.Column(db.String(20), unique=True, nullable=False)
     description = db.Column(db.String, unique=True, nullable=True)
@@ -25,17 +27,17 @@ class Sevices(db.Model):
     def __repr__(self):
         return f"User('{self.service_id}', '{self.service_name}')"
 
-    class Appointments(db.Model):
-    '''Customer Appointments'''
+
+class Appointments(db.Model):
+    """Customer Appointments"""
+
     appointment_id = db.Column(db.Integer, primary_key=True)
     which_service = db.Column(db.String(20), unique=True, nullable=False)
     details = db.Column(db.String, unique=True, nullable=True)
     which_customer = db.Column(db.DateTime, unique=True, nullable=False)
-    appointment_time = db.Column(db.DateTime, unique=True, nullable=False, default=datetime.utcnow)
+    appointment_time = db.Column(
+        db.DateTime, unique=True, nullable=False, default=datetime.utcnow
+    )
 
     def __repr__(self):
         return f"User('{self.service_id}', '{self.service_name}')"
-
-
-
-
