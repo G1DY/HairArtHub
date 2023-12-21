8616c770fd13637e3da33ebf4efbdb97413ba97d
jaminbenjamins <jaminbenjamins@gmail.com>
2023-12-21 11:03:07 -0800

    Handling login details.

---
+++ hairArtProject/FrontEnd/hair-art/src/components/Registeration.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./styles.css";
+import SignInForm from "./Signin";
+import SignUpForm from "./Signup";
+
+export default function App() {
+  const [type, setType] = useState("signIn");
+  const handleOnClick = text => {
+    if (text !== type) {
+      setType(text);
+      return;
+    }
+  };
+  const containerClass =
+    "container " + (type === "signUp" ? "right-panel-active" : "");
+  return (
+    <div className="App">
+      <h2>Log in or Sign up</h2>
+      <div className={containerClass} id="container">
+        <SignUpForm />
+        <SignInForm />
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To access your information and bookings. Please signin
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => handleOnClick("signIn")}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, There!</h1>
+              <p>Enter your personal details and start your journey with us</p>
+              <button
+                className="ghost "
+                id="signUp"
+                onClick={() => handleOnClick("signUp")}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
