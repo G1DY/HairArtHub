9def26ce86b61a3383cac487cd523ebe21c3f617
jaminbenjamins <jaminbenjamins@gmail.com>
2024-01-05 03:07:36 -0800

    Adding the correct path to file

--- hairArtProject/FrontEnd/hair-art/src/components/registration.js
+++ hairArtProject/FrontEnd/hair-art/src/components/registration.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LoginPage from './loginPage';
-import SignupPage from './Signup.js';
+import SignupPage from './signUp.js';
 import './Registration.css'; 
 
 
